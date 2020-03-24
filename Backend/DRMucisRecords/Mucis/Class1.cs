863c65424b57917fa6d3e54fd46f54239e8d1767
Nikolaj Dufke <nikolajdufke@live.dk>
2020-03-24 09:34:33 +0100

    record class

---
+++ Backend/DRMucisRecords/Mucis/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mucis
+{
+    public class Record
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public TimeSpan Duration { get; set; }
+        public int YearOfPublication { get; set; }
+        public int NumberOfSongs { get; set; }
+    }
+}
