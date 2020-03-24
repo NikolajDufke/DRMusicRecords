329b32216c254447dbe9518e259bd8ecbb2cb524
Nikolaj Dufke <nikolajdufke@live.dk>
2020-03-24 13:10:48 +0100

    import from books

--- Frontend/src/js/index.ts
+++ Frontend/src/js/index.ts
@@ -1,12 +1,124 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import Axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+
+import {json2table100} from "./genericTable";
+
+let BaseUri: string = "http://anbo-bookstorerest.azurewebsites.net/api/"
+let contentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+let element: HTMLDivElement = <HTMLDivElement> document.getElementById("table_content");
+let AllBooks: JSON;
+
+
+
+interface IBook {
+   id: number,
+   title: string,
+   author: string,
+   publisher: string,
+   price: number,
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let newBookElement: HTMLDivElement = <HTMLDivElement> document.getElementById("newBook");
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let newBookinputFields: string 
+
+
+
+
+
+// let book1 = Object.create(book)
+// console.log(Object.keys(book1));
+// // Object.keys(book).forEach(element => {
+// //     console.log(element); 
+// // });
+
+
+
+newBookinputFields = "<div class='container'>"+  MakeInputFields("Id") + MakeInputFields("title") + MakeInputFields("author") + MakeInputFields("publisher") + MakeInputFields("price") + "</div>";
+
+
+console.log(MakeInputFields("title"))
+
+newBookElement.innerHTML = newBookinputFields;
+
+ 
+
+
+function MakeInputFields(Name: string): string {
+    return "<div class='row'>"+ "<span class='col' id=' title" + Name + "'> " + Name +" </span> <input class='col' id=' input" + Name + "'></input> <span class='col'></span>" + "</div>";
+}
+
+// let sentence: string = `Hello, my name is ${ fullName }
+
+console.log("Getting localitems");
+Axios.get("https://restapidemo.azurewebsites.net/api/localitems").then(
+    function(response :AxiosResponse) : void{
+      
+        console.log(response.data)
+    }
+)
+.catch(
+    function(error: AxiosError):void{
+        console.log(error.message)
+        console.log(error.response)
+        console.log(error.stack)
+
+    }
+)
+
+
+ Axios.get(BaseUri + "Books").then(
+     function(respone: AxiosResponse): void{
+         console.log("getting books... v15");
+         console.log(Date.now)
+        //  element.innerHTML = "<div class='spinner-border text-muted></div>";
+        //  setTimeout(() => {  console.log("waited 5 sek"); }, 5000);
+    let data: IBook[] = respone.data;
+        console.log(data);
+    let result: string = json2table100(data);
+console.log(result);
+     element.innerHTML = result;
+        // contentElement.innerHTML = JSON.stringify(respone.data);
+     }
+ )
+ .catch(
+     function(error: AxiosError): void{
+        contentElement.innerHTML = error.message;
+     }
+ )
+
+let buttonElement: HTMLButtonElement = <HTMLButtonElement> document.getElementById("addButton");
+buttonElement.addEventListener("click", addBook);
+
+ function addBook(): void {
+    let id : HTMLInputElement = <HTMLInputElement> document.getElementById("inputid");
+    let title : HTMLInputElement = <HTMLInputElement> document.getElementById("inputtitle");
+    let author : HTMLInputElement = <HTMLInputElement> document.getElementById("inputauthor");
+    let publisher : HTMLInputElement = <HTMLInputElement> document.getElementById("inputpublisher");
+    let price : HTMLInputElement = <HTMLInputElement> document.getElementById("inputprice");
+    
+
+Axios.post(BaseUri + 'Books', { 
+    id : id.valueAsNumber, 
+    title : title.value,
+    author : author.value,
+    publisher : publisher.value,
+    price : price.valueAsNumber
+}).then(function (response){
+    console.log(response.data)
+}).catch(function(error){
+    console.log(error)
+});
+
+
+ }
+
+
+
+ 
+
+
+
+
