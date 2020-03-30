d67283bb30b1f08487c8829f675bc99fb590cce2
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2020-03-30 13:01:28 +0200

    tests

--- Backend/DRMusicRecord/UnitTestDRMusicRecords/FrontendTest.cs
+++ Backend/DRMusicRecord/UnitTestDRMusicRecords/FrontendTest.cs
@@ -16,8 +16,8 @@ namespace UnitTestDRMusicRecords
     {
         private static string _driverPath = "C:\\SeleniumDrivers";
         private static IWebDriver _driver;
-        private static string _webPage = "http://localhost:3000/";
-        //private static string _webPage = "https://drrecordswebpage.azurewebsites.net/";
+        //private static string _webPage = "http://localhost:3000/";
+        private static string _webPage = "https://drrecordswebpage.azurewebsites.net/";
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
@@ -81,7 +81,7 @@ namespace UnitTestDRMusicRecords
         [TestMethod]
         public void TestYearSearch()
         {
-            IWebElement buttonSearchelement = _driver.FindElement(By.Id("searchButton"));
+            IWebElement buttonSearchElement = _driver.FindElement(By.Id("searchButton"));
             IWebElement textElement = _driver.FindElement(By.Id("searchText"));
             IWebElement selectElement = _driver.FindElement(By.Id("searchSelect"));
             SelectElement selec = new SelectElement(selectElement);
@@ -90,11 +90,44 @@ namespace UnitTestDRMusicRecords
 
             selec.SelectByText("Year of Publication");
 
-            buttonSearchelement.Click();
+            buttonSearchElement.Click();
 
             IWebElement FirstResult = getWait().Until(d => d.FindElement(By.Id("tablecontent")));
             IList<IWebElement> tableRow = FirstResult.FindElements(By.TagName("tr"));
             Assert.AreEqual(8, tableRow.Count());
+        }
+
+        [TestMethod]
+        public void TestAddRecord()
+        {
+            
+            //Arrange
+
+            IWebElement buttonAddElement = _driver.FindElement(By.Id("addButton"));
+            IWebElement titleInputElement = _driver.FindElement(By.Id("titleInput"));
+            IWebElement authorInputElement = _driver.FindElement(By.Id("authorInput"));
+            IWebElement durationInputElement = _driver.FindElement(By.Id("durationInput"));
+            IWebElement yearofpublicationInputElement = _driver.FindElement(By.Id("yearofpublicationInput"));
+
+            titleInputElement.SendKeys("testrecord");
+            authorInputElement.SendKeys("BenjaminWS");
+            durationInputElement.SendKeys("150");
+            yearofpublicationInputElement.SendKeys("2020");
+
+            IWebElement TableContent = getWait().Until(d => d.FindElement(By.Id("tablecontent")));
+            IList<IWebElement> tableRow = TableContent.FindElements(By.TagName("tr"));
+            int countBefore = tableRow.Count();
+            //ACT
+
+            buttonAddElement.Click();
+
+            //Assert
+
+            TableContent = getWait().Until(d => d.FindElement(By.Id("tablecontent")));
+            tableRow = TableContent.FindElements(By.TagName("tr"));
+            int countAfter = tableRow.Count();
+
+            Assert.AreEqual(countBefore + 1, countAfter);
         }
     }
 }
--- Backend/DRMusicRecord/UnitTestDRMusicRecords/UnitTestAPI.cs
+++ Backend/DRMusicRecord/UnitTestDRMusicRecords/UnitTestAPI.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Music;
+using Newtonsoft.Json;
 using RestAPI.Controllers;
 
 namespace UnitTestDRMusicRecords
@@ -15,7 +17,7 @@ namespace UnitTestDRMusicRecords
         [TestInitialize]
         public void init()
         {
-           recordController = new RestAPI.Controllers.RecordController();
+            recordController = new RestAPI.Controllers.RecordController();
         }
 
         [TestMethod]
@@ -31,5 +33,123 @@ namespace UnitTestDRMusicRecords
             int actual = recordController.GetFromTitle("Stjerneskud").Count();
             Assert.AreEqual(1, actual);
         }
+
+        #region AddRecordTest
+
+        private Record GetRecord(string title = "test1")
+        {
+            return new Record()
+            {
+                Artist = "test",
+                Duration = 150,
+                Title = title,
+                YearOfPublication = 2020
+            };
+        }
+
+
+        [TestMethod]
+        public void PostRecordTest()
+        {
+
+            //Arrange
+             Record newRecord = GetRecord();
+
+            int countBefore = recordController.Get().Count();
+
+            //Act
+
+            recordController.Post(JsonConvert.SerializeObject(newRecord));
+
+            int countAfter = recordController.Get().Count();
+
+            //Assert
+
+            Assert.AreEqual(countBefore + 1, countAfter);
+
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void PostRecordMinCharTest()
+        {
+            //Arrange
+            Record newRecord = GetRecord("b");
+
+           
+            //Act 
+            recordController.Post(JsonConvert.SerializeObject(newRecord));
+
+        }
+
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void PostRecordMaxCharTest()
+        {
+            string testString = "EtHunredeOgEnChars";
+
+            for (int i = 0; i < 101 - testString.Length; i++)
+            {
+                testString += "-";
+            }
+
+            //Arrange
+            Record newRecord = GetRecord(testString);
+
+            //Act 
+            recordController.Post(JsonConvert.SerializeObject(newRecord));
+        }
+
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void PostRecordNoEmptyTest()
+        {
+         
+            //Arrange
+            Record newRecord = GetRecord("");
+
+            //Act 
+            recordController.Post(JsonConvert.SerializeObject(newRecord));
+        }
+
+        [TestMethod]
+        public void PostRecordMellemrumTest()
+        {
+
+            //Arrange
+            Record newRecord = GetRecord("Test test");
+
+            int countBefore = recordController.Get().Count();
+
+            //Act
+
+            recordController.Post(JsonConvert.SerializeObject(newRecord));
+
+            int countAfter = recordController.Get().Count();
+
+            //Assert
+
+            Assert.AreEqual(countBefore + 1, countAfter);
+
+        }
+
+        [ExpectedException(typeof(Exception))]
+        [TestMethod]
+        public void PostRecordSameRecordTwiceTest()
+        {
+
+            //Arrange
+            Record newRecord = GetRecord();
+            recordController.Post(JsonConvert.SerializeObject(newRecord));
+
+            //Act
+            recordController.Post(JsonConvert.SerializeObject(newRecord));
+        }
+
+
+        #endregion
     }
+
 }
